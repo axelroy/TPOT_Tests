4c6f4c4d8c06bdd4c14af854bee50e955b4396c7
axelroy <royaxe@gmail.com>
2017-05-23 10:22:47 +0200

    Separation of how to record the best pipeline, and how to access the set of explored pipelines

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # TPOT_Tests
+
+Repository to store the different experiments made with the TPOT library.
+
+TPOT is actually under development, the current version is the 0.7.5, so the
+following scripts might not work with the following versions.
+
+Tests done :
+
+* How to retrieve the different explored models. /retrieve_best_pipeline
+* How does the export work, and what can we do with it. /export_example
+
+To do tests :
+
+* How to find the features used for the pipeline optimisation, and can we find from
+  what the constructed ones have been made of.
+  
--- retrieve_best_pipeline/any_pipeline.py
+++ retrieve_best_pipeline/any_pipeline.py
@@ -10,7 +10,7 @@ iris = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(iris.data.astype(np.float64),
     iris.target.astype(np.float64), train_size=0.75, test_size=0.25)
 
-tpot = TPOTClassifier(generations=3, population_size=50, verbosity=2)
+tpot = TPOTClassifier(generations=1, population_size=50, verbosity=2)
 tpot.fit(X_train, y_train)
 print("Score of the best model found", tpot.score(X_test, y_test), "\n")
 
@@ -18,13 +18,16 @@ print("Score of the best model found", tpot.score(X_test, y_test), "\n")
 print("Part of the dict", dict(list(tpot._evaluated_individuals.items())[0:2]))
 
 # print a pipeline and its values
-best_pipeline = tpot._optimized_pipeline
+pipeline_str = list(tpot._evaluated_individuals.keys())[0]
+print("First dict entry config ", pipeline_str, "\n")
+print("First dict entry value ", tpot._evaluated_individuals[pipeline_str], "\n")
 
-# convert the best TPOT pipeline to scikit-learn pipeline object
-fitted_pipeline = tpot._toolbox.compile(expr=best_pipeline)
+# convert pipeline string to scikit-learn pipeline object
+optimized_pipeline = creator.Individual.from_string(pipeline_str, tpot._pset) # deap object
+fitted_pipeline = tpot._toolbox.compile(expr=optimized_pipeline ) # scikit-learn pipeline object
 
 # print scikit-learn pipeline objectP
-print("Best Pipeline :", fitted_pipeline, "\n")
+print("Chosen Pipeline :", fitted_pipeline, "\n")
 
 # Fix random state when the operator allows  (optional) just for get consistent CV score
 # This allows to set the random set to always have the same training/test sets
@@ -32,5 +35,5 @@ tpot._set_param_recursive(fitted_pipeline.steps, 'random_state', 42)
 
 # CV scores from scikit-learn
 scores = cross_val_score(fitted_pipeline, X_train, y_train, cv=5, scoring='accuracy', verbose=0)
-print("Scores  \n", np.mean(scores))
-print("Scores  \n", tpot._evaluated_individuals[best_pipeline][1])
+print("Calculated Score  \n", np.mean(scores))
+print("TPOT Score  \n", tpot._evaluated_individuals[pipeline_str][1])
